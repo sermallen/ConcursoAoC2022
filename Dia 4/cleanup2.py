89218bbdbaa380ba25675482098b504a0c519c39
sermallen <ser.mallen@gmail.com>
2022-12-06 22:17:48 +0100

    Dia 4

---
+++ Dia 4/cleanup2.py
@@ -0,0 +1,20 @@
+fulloverlaps = 0
+with open("Dia 4/input.txt") as file:
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        line = line.split(sep=",")
+        pair1 = line[0].split("-")
+        pair2 = line[1].split("-")
+
+        if ((int(pair1[0]) <= int(pair2[0]) and int(pair1[1]) >= int(pair2[1]))
+         or (int(pair1[0]) >= int(pair2[0]) and int(pair1[1]) >= int(pair2[1]))
+         or (int(pair1[1]) >= int(pair2[0]) and int(pair1[1]) <= int(pair2[1]))
+         or (int(pair1[0]) >= int(pair2[1]) and int(pair1[0]) <= int(pair2[1]))):
+            fulloverlaps += 1
+        # es una chapuza pero estoy hasta los cojones de esta pregunta la vd
+        if (int(pair1[0]) > int(pair2[0]) and int(pair1[0]) > int(pair2[1])):
+            fulloverlaps -= 1
+
+print(fulloverlaps)                
